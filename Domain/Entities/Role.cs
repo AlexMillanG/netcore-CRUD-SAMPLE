050ce55ed5bb84faf73a67b092c2d93e43e62a70
AlexMillanG <alexmillanguillen@gmail.com>
2026-05-12 15:12:05 -0600

    fix: role name initialized as empty

--- Domain/Entities/Role.cs
+++ Domain/Entities/Role.cs
@@ -5,7 +5,7 @@ namespace SolucionChida.Domain.Entities;
 public class Role
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
 
     [JsonIgnore]
     public ICollection<User> Users { get; set; } = new List<User>();
